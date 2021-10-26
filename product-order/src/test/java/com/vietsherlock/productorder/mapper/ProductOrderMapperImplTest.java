1f7ebdb5fa661967ce609c0bbd26b2aebc9f05fd
Viet Le Nguyen Ngoc <lnnviet@IfiSolution.local>
2021-10-26 17:46:02 +0700

    continue update unit test

---
+++ product-order/src/test/java/com/vietsherlock/productorder/mapper/ProductOrderMapperImplTest.java
@@ -0,0 +1,20 @@
+package com.vietsherlock.productorder.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductOrderMapperImplTest {
+
+    @Test
+    void productOrderClientToServer() {
+    }
+
+    @Test
+    void productOrderCreateServerToClient() {
+    }
+
+    @Test
+    void createOrderResponseClientToServer() {
+    }
+}
